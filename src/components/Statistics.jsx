e9f1b6bb599c0e332fd83bf6c4cfdbeac879962c
AlexandraMarkova <a.markova@ukr.net>
2021-03-24 18:58:24 +0200

    фиксит второе заданме

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -4,14 +4,15 @@ import PropTypes from "prop-types";
 
 const Statistics = ({ title, stats }) => {
     return (
-<section className="statistics">
-        <h2 className="title">{title}</h2>
+        <section className="statistics">
+            {title && <h2 className="title">{title}</h2>}
+            
 
         <ul className="stat-list">
-                {stats.map((data) => (
-            <li key={data.id}>
-                <span className="label">{data.label} </span>
-                <span className="percentage">{data.percentage}</span>
+                {stats.map(({ id, label, percentage }) => (
+            <li key={id}>
+                <span className="label">{label} </span>
+                <span className="percentage">{percentage}</span>
             </li>
         ))}
         </ul>
@@ -20,13 +21,17 @@ const Statistics = ({ title, stats }) => {
     )  
 }
 
-// Profile.defaultProps = {
-//     title:,
-// };
+Statistics.defaultProps = {
+    title:"",
+};
 
 Statistics.propTypes = {
     title: PropTypes.string,
-    stats: PropTypes.array.isRequired,
+    stats: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired
+    })).isRequired,
 };
 
 export default Statistics;

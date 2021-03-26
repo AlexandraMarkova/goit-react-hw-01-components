61be69635de92623ef0781083871e8686917d331
AlexandraMarkova <a.markova@ukr.net>
2021-03-27 01:18:29 +0200

    Updates

---
+++ precache-manifest.ad5813150d81a28b910e2c5f6ec969ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc2e226e7d6d49f98d30078c9e3b0fc",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6c752d205348bee529a8",
+    "url": "/goit-react-hw-01-components/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "7dce14fb6bbabd8a7fb9",
+    "url": "/goit-react-hw-01-components/static/css/main.3187ee95.chunk.css"
+  },
+  {
+    "revision": "6c752d205348bee529a8",
+    "url": "/goit-react-hw-01-components/static/js/2.03afd8ce.chunk.js"
+  },
+  {
+    "revision": "7dce14fb6bbabd8a7fb9",
+    "url": "/goit-react-hw-01-components/static/js/main.e83bce4d.chunk.js"
+  },
+  {
+    "revision": "f638cb1d6a2ca084c696",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1d6ff9dc.js"
+  }
+]);
